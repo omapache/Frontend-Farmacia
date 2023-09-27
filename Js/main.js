4322c9be57f6415ee22de36c197b2c75cfd4f9eb
andresangarita10 <andres.tiikas@gmail.com>
2023-09-27 17:02:12 -0500

    feat: :lock: ya sirve el login por fiiiinnxddxdxdxdxdxxddxdxdx

--- Js/main.js
+++ Js/main.js
@@ -1,62 +1,43 @@
-const URL = "http://localhost:5115"
-const urlValidation = "/api/farmacia/User/validate-credentials"
-const headers = new Headers({'Content-Type': 'application/json'});
+const URL = "http://localhost:5115";
+const urlValidation = "/api/farmacia/User/validate-credentials";
+const headers = new Headers({ 'Content-Type': 'application/json' });
 const botonLogin = document.getElementById('botoncito');
 
-
 botonLogin.addEventListener("click", function (e) {
-    e.eventpreventDefault();
-    Validar2();
+    e.preventDefault(); // Evita el envío del formulario por defecto
+    Validar();
 });
 
-/* async function Validar() {
+
+async function Validar() {
+    debugger;
     const username = document.getElementById("username").value;
     const password = document.getElementById("password").value;
 
     const data = {
-        Username: username,
-        Password: password
+        "username": username,
+        "password": password
+    };
+    console.log(data);
+    const config = {
+        method: 'POST',
+        headers: headers,
+        body: JSON.stringify(data)
     };
 
     try {
-        const response = await fetch(`${url}/api/farmacia/User/validate-credentials`, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(data)
-        });
+        const response = await fetch(`${URL}${urlValidation}`, config);
 
         if (response.ok) {
-            const result = await response.json();
-            if (result === true) {
-                console.log("ENTROOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO");
-                 window.location.href = "../index.html";
-            } else {
-                console.error("Credenciales incorrectas");
-            }
+            console.log("ENTROOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO");
+            window.location.href = "../index.html";
         } else {
-            console.error("Error de red AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+            console.error("Error de red");
         }
     } catch (error) {
         console.error("Error de red: ", error);
     }
-} */
-
-async function Validar2() {
-    debugger
-    const username = document.getElementById("username").value;
-    const password = document.getElementById("password").value;
-    let data = {
-        "username": "user5",
-        "password": "password5"
-    }
-    let config = {
-        method: 'POST',
-        headers: headers,
-        body: JSON.stringify(data)
-    }
-    await fetch(`http://localhost:5115/api/farmacia/User/validate-credentials`,config).json();
 }
+
 
 
