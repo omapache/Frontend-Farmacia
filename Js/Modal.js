3d672d97222b996733512cfe2fac4819403b9c2f
omapache <owenblanco77@gmail.com>
2023-09-29 01:59:16 -0500

    feat: :sparkles: hize consultas 3, 6, 9, 12

---
+++ Js/Modal.js
@@ -0,0 +1,114 @@
+let botonCerrarModal = document.getElementById('CerrarModal');
+document.addEventListener('DOMContentLoaded', function() {
+    cargarProveedores();
+    cargarMedicamentos();
+});
+
+if (botonCerrarModal) {
+    botonCerrarModal.addEventListener('click', function () {
+        cerrarModal();
+    });
+}
+
+// Obtén el elemento select por su ID
+var select = document.getElementById('dropdownProveedor');
+
+if (select) {
+    select.addEventListener('change', function () {
+        abrirModal();
+    });
+}
+var selectm = document.getElementById('dropdownMedicamento');
+
+if (selectm) {
+    selectm.addEventListener('change', function () {
+        abrirModal();
+    });
+}
+let inputFecha = document.getElementById('input-fecha');
+
+if (inputFecha) {
+    inputFecha.addEventListener('input', function () {
+    abrirModal();
+    });
+}
+var inputAnio = document.getElementById('inputAnio');
+inputAnio.addEventListener('input', function () {
+    validarAnio();
+});
+
+function validarAnio() {
+    var anio = inputAnio.value;
+    if (anio.length === 4 && !isNaN(anio)) {
+        abrirModal();
+    } 
+}
+
+
+function cerrarModal() {
+    // Obtiene el elemento modal por su id
+    var modal = document.getElementById('staticBackdrop');
+
+    // Cierra el modal
+    if (modal) {
+        modal.classList.remove('show');
+        modal.style.display = 'none';
+        var modalTitle = document.getElementById('TituloResultadoConsultaInventario');
+        modalTitle.innerHTML = '';
+        var modalBody = document.getElementById('resultadoConsultaInventario');
+        modalBody.innerHTML = '';
+    }
+}
+
+function abrirModal() {
+    // Obtiene el elemento modal por su id
+    var modal = document.getElementById('staticBackdrop');
+    // Abre el modal
+    if (modal) {
+        modal.classList.add('show');
+        modal.style.display = 'block';
+    }
+}
+
+function cargarProveedores() {
+    fetch('http://localhost:5115/api/farmacia/persona')
+        .then(response => response.json()) // Parsear la respuesta JSON
+        .then(data => {
+            var proveedores = data.filter(proveedor => proveedor.rol.nombre == "Proveedor");
+            var select = document.getElementById('dropdownProveedor');
+            select.innerHTML = '';
+            var optionDefault = document.createElement('option');
+            optionDefault.textContent = 'Seleccione un proveedor';
+            select.appendChild(optionDefault);
+            proveedores.forEach(proveedor => {
+                var option = document.createElement('option');
+                option.value = proveedor.id; 
+                option.textContent = proveedor.nombre; 
+                select.appendChild(option);
+            });
+        })
+        .catch(error => {
+            console.error('Error al cargar los proveedores:', error);
+        });
+}
+function cargarMedicamentos() {
+    fetch('http://localhost:5115/api/farmacia/descripcionMedicamento')
+        .then(response => response.json()) // Parsear la respuesta JSON
+        .then(data => {
+            var Medicamentos = data
+            var select = document.getElementById('dropdownMedicamento');
+            select.innerHTML = '';
+            var optionDefault = document.createElement('option');
+            optionDefault.textContent = 'Seleccione un proveedor';
+            select.appendChild(optionDefault);
+            Medicamentos.forEach(proveedor => {
+                var option = document.createElement('option');
+                option.value = proveedor.id; 
+                option.textContent = proveedor.nombre; 
+                select.appendChild(option);
+            });
+        })
+        .catch(error => {
+            console.error('Error al cargar los proveedores:', error);
+        });
+}
